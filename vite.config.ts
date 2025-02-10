e02a14a5bd2376da38e7d599786648b00c78d8b5
MichaelWebCork <m.oconnell56@yahoo.ie>
2025-02-10 23:59:18 +0000

    working exporting to legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from "@tailwindcss/vite";
+import legacy from "@vitejs/plugin-legacy"; // Import it
+
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tailwindcss()],
+  plugins: [
+    svelte(),
+    tailwindcss(),
+    legacy({
+      targets: ["ie >= 10"], //Target IE 11, adjust as needed
+    }),
+  ],
 });
