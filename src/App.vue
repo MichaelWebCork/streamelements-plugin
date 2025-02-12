2ee3364f6a8251969e4fc4fd570323f9a091602f
MichaelWebCork <m.oconnell56@yahoo.ie>
2025-02-12 21:59:36 +0000

    initvue

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<style>
+  @import "tailwindcss";
+</style>
+<script setup lang="ts">  
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Vite + Vue" />
+</template>
